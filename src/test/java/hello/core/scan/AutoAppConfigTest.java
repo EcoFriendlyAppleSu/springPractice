8831c5c6dd3a29ac83eb2ce74115057e156941fd
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-09-22 21:20:16 +0900

    feat : bean strategy pattern

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -2,7 +2,9 @@ package hello.core.scan;
 
 import hello.core.AutoAppConfig;
 import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,6 +16,14 @@ public class AutoAppConfigTest {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
         MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+    @Test
+    public void memberServiceImplComponentTypeTest() throws Exception {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberServiceImpl", MemberServiceImpl.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
     }
 }
