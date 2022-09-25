1c320f7c8011ae1fce41b8e6f136a13bbaa4f874
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-09-25 21:00:38 +0900

    feat : request scope wit proxy

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,12 +14,14 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    // private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody //화면 없이 메세지를 반환하고 싶을 때 사용
     public String logDemo(HttpServletRequest request) { // 고객 요청 정보 받아옴
-        MyLogger myLogger = myLoggerProvider.getObject();
+        System.out.println("[LogDemoController.logDemo]" + this);
+        // MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString(); // get client input URL info
         myLogger.setRequestURL(requestURL);
 
